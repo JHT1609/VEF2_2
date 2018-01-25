f42a679b12c0cd3cbdc028fa40dd0ad018994c3e
JHT1609 <johannesto245@nemi.tskoli.is>
2018-01-25 14:15:45 +0000

    sections

--- project.py
+++ project.py
@@ -3,6 +3,15 @@ from bottle import route, run
 
 @route('/')
 def index():
-    return "Hello"
+    return "<a href='/section_a'>Liður A</a>" \
+            "<a href='/section_b'>Liður B</a>" \
 
 run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
+@route('/section_a')
+def a():
+    return "a"
+
+@route('/section_b')
+def b():
+    return "b"
