346117e208312d25141f716ee26f768eafad6aae
JHT1609 <johannesto245@nemi.tskoli.is>
2018-01-25 13:46:35 +0000

    Update bottle.py

--- bottle.py
+++ bottle.py
@@ -1,8 +1,8 @@
 import os
 from bottle import route, run
 
-@route('/hello')
+@route('/')
 def index():
-    return "Hello"
+    return 'Hello bottle'
 
 run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
